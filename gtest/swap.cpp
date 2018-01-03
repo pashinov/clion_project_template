f7b85b03276047d2c06809e2a39b4ff0b6fc08f1
Aleksei Pashinov <pashinov@outlook.com>
2018-01-03 22:25:57 +0300

    Added Google Test #2

---
+++ gtest/swap.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include <swap.hpp>
+
+TEST(BasicTest, SwapTest) {
+    std::string str1 = "Hello"; std::string str2 = "World";
+    cpp_project_template::swap(str1, str2);
+    ASSERT_EQ(str1, "World");
+    ASSERT_EQ(str2, "Hello");
+}
