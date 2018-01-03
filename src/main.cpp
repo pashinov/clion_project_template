c5ef0dc9773c5a85af169e25a543efac1ff4a900
Aleksei Pashinov <pashinov@outlook.com>
2018-01-03 22:25:57 +0300

    Added Google Test #2

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,11 @@
 
 int main(int argc, char* argv[])
 {
-    int a =10; int b =5;
+    std::string str1 = "Hello"; std::string str2 = "World";
 
-    cpp_project_template::swap(a, b);
+    cpp_project_template::swap(str1, str2);
 
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
+    std::cout << str2 << ", " << str1 << "!!!" << std::endl;
 
     return 0;
 }
